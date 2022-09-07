928a778cbee08c5c1c1271d66c893bbc5541eade
soc06212 <soc06212@gmail.com>
2022-09-07 18:41:14 +0900

    Feat: 자동 로그인과 초기 DB 생성을 LaunchActivity에서 처리

---
+++ app/src/main/java/com/mgchoi/smartportfolio/LaunchActivity.kt
@@ -0,0 +1,40 @@
+package com.mgchoi.smartportfolio
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.preference.PreferenceManager
+import com.mgchoi.smartportfolio.tool.DBManager
+import com.mgchoi.smartportfolio.value.SharedPreferenceKeys
+
+class LaunchActivity : AppCompatActivity() {
+
+    private lateinit var initializer: DBManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initializer = DBManager(this)
+
+//        initMemberDB()
+//        initPortfolioDB()
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        if (pref.getBoolean(SharedPreferenceKeys.BOOL_AUTO_LOGIN, false)) {
+            launchMainActivity()
+        } else {
+            launchLoginActivity()
+        }
+    }
+
+    private fun launchMainActivity() {
+        val intent = Intent(this@LaunchActivity, MainActivity::class.java)
+        startActivity(intent)
+        this.finish()
+    }
+
+    private fun launchLoginActivity() {
+        val intent = Intent(this@LaunchActivity, LoginActivity::class.java)
+        startActivity(intent)
+        this.finish()
+    }
+
+}
