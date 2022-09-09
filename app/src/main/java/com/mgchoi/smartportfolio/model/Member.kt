79dc139778b4f7172d8643e01ad7235585e61a62
soc06212 <soc06212@gmail.com>
2022-09-09 17:51:11 +0900

    Feat: Member 객체 Serializable 추가

--- app/src/main/java/com/mgchoi/smartportfolio/model/Member.kt
+++ app/src/main/java/com/mgchoi/smartportfolio/model/Member.kt
@@ -1,6 +1,6 @@
 package com.mgchoi.smartportfolio.model
 
-import com.mgchoi.smartportfolio.ViewStyle
+import java.io.Serializable
 
 class Member(
     var id: Int,
@@ -9,4 +9,4 @@ class Member(
     var url: String,
     var viewStyle: ViewStyle,
     var destroyable: Boolean
-)
+) : Serializable
