98278ffa4fabc3ffb16e2d68a99cb4c4898c9a97
soc06212 <soc06212@gmail.com>
2022-12-08 16:51:21 +0900

    Feat: SplashScreen Icon을 Animated Vector Drawable로 변경

--- app/src/main/java/com/mgchoi/smartportfolio/LaunchActivity.kt
+++ app/src/main/java/com/mgchoi/smartportfolio/LaunchActivity.kt
@@ -5,15 +5,24 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.ViewTreeObserver.OnPreDrawListener
 import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.preference.PreferenceManager
 import com.mgchoi.smartportfolio.tool.DBManager
 import com.mgchoi.smartportfolio.value.SharedPreferenceKeys
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class LaunchActivity : AppCompatActivity() {
 
     private lateinit var initializer: DBManager
+    private var splashAnimationDone = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // Android 12 Splash Screen
@@ -31,7 +40,27 @@ class LaunchActivity : AppCompatActivity() {
         } else {
             launchLoginActivity()
         }
+
+        // 애니메이션이 끝나야 Splash가 종료되도록 viewTreeObserver 추가
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(object : OnPreDrawListener {
+            override fun onPreDraw(): Boolean {
+                return if (splashAnimationDone) {
+                    content.viewTreeObserver.removeOnPreDrawListener(this)
+                    true
+                } else {
+                    false
+                }
+            }
+        })
+
+        // 애니메이션 500ms 동안 지속
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(1000)
+            splashAnimationDone = true
+        }
     }
+
 
     private fun launchMainActivity() {
         val intent = Intent(this@LaunchActivity, MainActivity::class.java)
